478b3728fa519ce377fc7d46bd2525fc75428d53
Fernando Bataglin <fernando.bataglin@hotmail.com>
2018-12-04 21:49:22 -0200

    Nova tela de inicio

--- src/assets/js/inicio.js
+++ src/assets/js/inicio.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
-    $('.navbar-toggle').on('click', function (e) {
-      $('.side-nav').toggleClass("open");
-      $('#wrapper').toggleClass("open");
-      e.preventDefault();
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
     });
-  });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
