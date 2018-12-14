6066c6e26920d8ac2b35a2708858fa93dbc15631
Edison <edison_schulz@estudante.sc.senai.br>
2018-12-13 22:01:12 -0200

    ajustes menu

--- Resomar/WebContent/modulos/app.js
+++ Resomar/WebContent/modulos/app.js
@@ -1,11 +1,19 @@
 angular.module('app', ['ngRoute']);
 
 angular.module('app').config(function($httpProvider, $routeProvider, $locationProvider) {
+	console.log($routeProvider);
+	
 	$locationProvider.hashPrefix('');
 	$routeProvider.when('/', {
 		templateUrl : '../home/home.html'
 //		controller : 'homeController',
 //		controllerAs : 'homeCtrl'
+	})
+	.when('/login', {
+		templateUrl : '../login/login.html'
+//		controller : 'homeController',
+//		controllerAs : 'homeCtrl'
+
 	})
 	.otherwise({
 		redirectTo : '/'
