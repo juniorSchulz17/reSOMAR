bd0ab5eae3fa109d039dbb100314ef4426505b3e
Edison <edison_schulz@estudante.sc.senai.br>
2019-02-12 22:13:19 -0200

    ajustes nas rotas e menu

--- Resomar/WebContent/modulos/app.js
+++ Resomar/WebContent/modulos/app.js
@@ -1,46 +1,36 @@
-angular.module('app', [ 'ngRoute' ]);
+angular.module('app', ['ngRoute']);
 
 angular.module('app').config(
-		function($httpProvider, $routeProvider, $locationProvider) {
-			$locationProvider.hashPrefix('');
+    function ($httpProvider, $routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
 
-			$routeProvider.when('/', {
-				templateUrl : 'View/home/home.html'
-					// controller : 'homeController',
-					// controllerAs : 'homeCtrl'
-			}).when('/login', {
-				templateUrl : 'View/login/login.html'
-					// controller : 'homeController',
-					// controllerAs : 'homeCtrl'
+        $routeProvider.when('/', {
+            templateUrl: 'View/home/home.html'
+            // controller : 'homeController',
+            // controllerAs : 'homeCtrl'
+        }).when('/login', {
+            templateUrl: 'View/login/login.html'
+            // controller : 'homeController',
+            // controllerAs : 'homeCtrl'
 
-			}).when('/index	', {
-				templateUrl : 'View/home/home.html'
-					// controller : 'homeController',
-					// controllerAs : 'homeCtrl'
+        }).when('/index	', {
+            templateUrl: 'View/home/home.html'
+            // controller : 'homeController',
+            // controllerAs : 'homeCtrl'
 
-			}).otherwise({
-				redirectTo : '/'
-			});
-		});
+        }).otherwise({
+            redirectTo: '/'
+        });
+    });
 
 angular.module('app').directive(
-		'head',['$rootScope','$location','$cookies','$window', function($rootScope, $location, $cookies, $window) {
-				return {
-					restrict : 'E',
-
-					link : function(scope, elem) {
-						$rootScope.$on('$routeChangeStart',function(e, next, current) {
-									$rootScope.activeTab = $location.path();
-									$rootScope.currentUser = JSON.parse($window.localStorage.getItem('currentUser'));
-									if (next && next.$$route) {
-										if (next.$$route.originalPath !== '/login') {
-											if (!$cookies.get('login')) {
-												$location.path('/login');
-											}
-										}
-									}
-
-								});
-					}
-				};
-			} ]);
+    'head', ['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
+        return {
+            restrict: 'E',
+            link: function (scope, elem) {
+                $rootScope.$on('$routeChangeStart', function (e, next, current) {
+                    $rootScope.activeTab = $location.path();
+                });
+            }
+        };
+    }]);
