0feb87e0db1a1d8988786ee56f8ef1495fa18f83
Fernando Bataglin <fernando.bataglin@hotmail.com>
2019-02-18 12:37:56 -0300

    Menu completo

--- Resomar/WebContent/assets/js/grafic.js
+++ Resomar/WebContent/assets/js/grafic.js
@@ -1,61 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    carregarMes();
-    grafico();
-
-    var jan;
-    var fev;
-    var mar;
-    var abr;
-    var mai;
-    var jun;
-    var jul;
-    var ago;
-    var set;
-    var out;
-    var nov;
-    var dez;
-
-    function carregarMes() {
-        $.ajax({
-            type: 'POST',
-            url: "/TMLConstrucoes/rest/grafico/buscarMeses",
-            success: function(retorno) {
-                var retorno = JSON.parse(retorno);
-                mes = retorno;
-                grafico(mes);
-            },
-            error: function() {
+    new Chart(document.getElementById("pie-chart"), {
+        type: 'pie',
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [{
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                data: [2478,5267,734,784,433]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
             }
-        });
-    };
+        }
+    });
 
-        var ctx = document.getElementById("myChart").getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto","Setembro", "Outubro", "Novembro", "Dezembro"],
-                datasets: [{
-                    label: 'Gastos Mensais',
-                    data: [jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez], //Criar uma lista para cada mes e mandar nessa budega
-                    backgroundColor: [
-                        'rgba(25, 89, 148, 0.82)'
-                    ],
-                    borderColor: [
-                        'rgba(0,0,0,1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
+    new Chart(document.getElementById("doughnut-chart"), {
+        type: 'doughnut',
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [
+                {
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                    data: [2478,5267,734,784,433]
                 }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
             }
-        });
-    }
-});
+        }
+    });
+
+})
