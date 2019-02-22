89ea5566bac08981dec5b3dc92e7f0f3fd3a014b
Edison <edison_schulz@estudante.sc.senai.br>
2019-02-21 21:06:13 -0300

    finalizado login

--- Resomar/WebContent/modules/login/login.controller.js
+++ Resomar/WebContent/modules/login/login.controller.js
@@ -17,3 +17,18 @@ function loginController() {
         return false;
     }
 }
+
+angular.module('app').directive('focus', function($timeout, $parse) {
+    return {
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.focus);
+            scope.$watch(model, function(value) {
+                if(value === true) {
+                    $timeout(function() {
+                        element[0].focus();
+                    });
+                }
+            });
+        }
+    };
+});
